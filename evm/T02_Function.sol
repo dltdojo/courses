060c5089d70353d3f26b8532300b20228e753e8f
dltdojo <y12studio@gmail.com>
2017-08-13 17:54:06 +0800

    add evm/js

--- evm/T02_Function.sol
+++ evm/T02_Function.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.4.15;
 
 contract Foo { uint foo = 5;}
-contract FooPublic { uint public foo = 5;}
-contract FooGet {uint foo = 5;  function get() constant returns (uint) {return foo;}}
-contract FooGetAdd {uint foo = 5;  function get() constant returns (uint) {return foo+5;}}
-contract FooGetParamMul {function get(uint a) constant returns (uint) {return a*5;}}
-contract FooGet2Functions {
+contract T01Public { uint public foo = 5;}
+contract T02Get {uint foo = 5;  function get() constant returns (uint) {return foo;}}
+contract T03GetAdd {uint foo = 5;  function get() constant returns (uint) {return foo+5;}}
+contract T04GetParamMul {function get(uint a) constant returns (uint) {return a*5;}}
+contract T05Get2Functions {
     function getMul(uint a) constant returns (uint) {return a*5;}
     function getAdd(uint a) constant returns (uint) {return a+5;}
 }
